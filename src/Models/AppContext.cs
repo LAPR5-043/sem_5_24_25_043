8bd8bee890d019f29586e8d79a92e2c710c2b39d
Diogo Campos <1220941@isep.ipp.pt>
2024-10-17 14:00:53 +0100

    #2 Refactoring implementation

--- src/Models/AppContext.cs
+++ src/Models/AppContext.cs
@@ -1,6 +1,5 @@
 using Microsoft.EntityFrameworkCore;
 using src.Domain;
-using src.Infrastructure.OperationTypes;
 
 namespace src.Models;
 
@@ -11,29 +10,21 @@ public class AppContext : DbContext
     {
     }
 
-    public DbSet<OperationType> OperationTypes { get; set; } = null!;
-    public DbSet<Specialization> Specializations { get; set; } = null!;
-    public DbSet<Staff> Staffs { get; set; } = null!;
+    //public DbSet<OperationType> OperationTypes { get; set; } = null!;
+    //public DbSet<Specialization> Specializations { get; set; } = null!;
+    //public DbSet<Staff> Staffs { get; set; } = null!;
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        modelBuilder.ApplyConfiguration(new OperationTypeEntityTypeConfiguration());
-        modelBuilder.ApplyConfiguration(new SpecializationEntityTypeConfiguration());
-        modelBuilder.ApplyConfiguration(new StaffEntityTypeConfiguration());
 
         // Seed data
         string cardiologyId = "Cardiology";
         string neurologyId = "Neurology";
 
-        modelBuilder.Entity<Specialization>().HasData(
-            new Specialization("Cardiology", "Heart specialist") { Id = new SpecializationName(cardiologyId) },
-            new Specialization("Neurology", "Brain specialist") { Id = new SpecializationName(neurologyId) }
-        );
+        //modelBuilder.Entity<Specialization>().HasData(
+      
+        //modelBuilder.Entity<Staff>().HasData(
 
-        modelBuilder.Entity<Staff>().HasData(
-            new Staff("John", "Doe", "N202100001", true, "923456890", "johndoe@example.com", cardiologyId, null) { Id = new LicenseNumber("N202100001") } ,
-            new Staff("Jane", "Smith", "D202100002", true, "987654210", "janesmith@example.com", neurologyId, null) { Id = new LicenseNumber("D202100002") });
-  
 
     }
 
