673dca108844e9ba479637ae987b49f0a01ee800
Lucas Gonçalves <lucasfgon29@gmail.com>
2024-10-22 00:40:46 +0100

    #22 Added Dto method

--- src/Domain/OperationRequestAggregate/OperationRequestDto.cs
+++ src/Domain/OperationRequestAggregate/OperationRequestDto.cs
@@ -1,23 +1,42 @@
-using System;
 using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+using Domain.OperationRequestAggregate;
 
-    public class OperationRequestDto
+public class OperationRequestDto
+{
+
+    public string OperationRequestID { get; set; }
+
+    public string PatientID { get; set; }
+
+    public string DoctorID { get; set; }
+
+    public string OperationTypeID { get; set; }
+
+    public string DeadlineDate { get; set; }
+
+    public string Priority { get; set; }
+
+    public OperationRequestDto(OperationRequest operationRequest)
     {
-        
-        public long RequestId { get; set; }
 
-        public int patientID { get; set; }
-
-        public string operationType { get; set; }
-
-        public string doctorID { get; set; }
-        public string priority { get; set; }
-
-        public int day { get; set; }
-
-        public int month { get; set; }
-
-        public int year { get; set; }
-
-
+        this.OperationRequestID = operationRequest.operationRequestID.AsString();
+        this.PatientID = operationRequest.patientID.ToString();
+        this.DoctorID = operationRequest.doctorID.ToString();
+        this.OperationTypeID = operationRequest.operationTypeID.ToString();
+        this.DeadlineDate = operationRequest.deadlineDate.ToString();
+        this.Priority = operationRequest.priority.ToString();
     }
+
+    [JsonConstructor]
+    public OperationRequestDto(string OperationRequestID, string PatientID, string DoctorID, string OperationTypeID, string DeadlineDate, string Priority)
+    {
+        this.OperationRequestID = OperationRequestID;
+        this.PatientID = PatientID;
+        this.DoctorID = DoctorID;
+        this.OperationTypeID = OperationTypeID;
+        this.DeadlineDate = DeadlineDate;
+        this.Priority = Priority;
+    }
+
+}
