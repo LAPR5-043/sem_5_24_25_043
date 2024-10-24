9ca8c6646661ffea590f6c7a6cc4bde2634e1c0d
Ricardo <1220606@isep.ipp.pt>
2024-10-24 09:36:42 +0100

    #6 Update AppSettings

--- src/obj/Debug/net8.0/sem_5_24_25_043.AssemblyInfo.cs
+++ src/obj/Debug/net8.0/sem_5_24_25_043.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("sem_5_24_25_043")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12e5b2f3002708c62eb4b9e44e88d5b98e4687d0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+760d81db1c36954cdf7aed81c2584bc55844a97a")]
 [assembly: System.Reflection.AssemblyProductAttribute("sem_5_24_25_043")]
 [assembly: System.Reflection.AssemblyTitleAttribute("sem_5_24_25_043")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
