e4a62f9ca5d225005808fcaa07d35adf7f52a8c3
Ricardo <1220606@isep.ipp.pt>
2024-10-25 15:24:46 +0100

    #21 Service Integration Tests

--- src/obj/Debug/net8.0/sem_5_24_25_043.AssemblyInfo.cs
+++ src/obj/Debug/net8.0/sem_5_24_25_043.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("sem_5_24_25_043")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8fc8c6c3cc751e4b2256e769b99a5e79f643e647")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9206d90d7fd36c5561ebc5657dedd10a49012164")]
 [assembly: System.Reflection.AssemblyProductAttribute("sem_5_24_25_043")]
 [assembly: System.Reflection.AssemblyTitleAttribute("sem_5_24_25_043")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
