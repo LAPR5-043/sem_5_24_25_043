936859f381dae431b744aa18dd3b2b4844c570b3
Ricardo <1220606@isep.ipp.pt>
2024-10-25 10:53:23 +0100

    #11 Integration Tests for Controller

--- Tests/IntegrationTests/PatientControllerTests.cs
+++ Tests/IntegrationTests/PatientControllerTests.cs
@@ -13,18 +13,96 @@ namespace src.IntegrationTests
 {
     public class PatientControllerTests
     {
+        private readonly Mock<IPatientService> mockPatientService;
+        private readonly PatientController controller;
+
+
+        public PatientControllerTests()
+        {
+            mockPatientService = new Mock<IPatientService>();
+            controller = new PatientController(mockPatientService.Object);
+        }
+
+        [Fact]
+        public async Task CreatePatient_ShouldReturnCreatedStatus()
+        {
+            // Arrange
+            var patientDto = new PatientDto
+            {
+                MedicalRecordNumber = "P123",
+                FirstName = "John",
+                LastName = "Doe",
+                Email = "john.doe@example.com",
+                PhoneNumber = "+1234567890",
+                EmergencyContactName = "Jane Doe",
+                EmergencyContactPhoneNumber = "+351987654321",
+                DayOfBirth = "1",
+                MonthOfBirth = "1",
+                YearOfBirth = "2000",
+                Gender = "Male"
+            };
+
+            mockPatientService.Setup(service => service.CreatePatientAsync(It.IsAny<PatientDto>()))
+                              .ReturnsAsync(patientDto);
+
+            // Act
+            var result = await controller.CreatePatient(patientDto);
+
+            // Assert
+            var createdResult = Assert.IsType<CreatedAtActionResult>(result);
+            Assert.Equal(nameof(controller.GetPatientById), createdResult.ActionName);
+            Assert.Equal(patientDto, createdResult.Value);
+        }
+
+        [Fact]
+        public async Task CreatePatient_ShouldReturnBadRequestStatus()
+        {
+            // Act
+            var result = await controller.CreatePatient(null);
+
+            // Assert
+            var badRequestResult = Assert.IsType<BadRequestObjectResult>(result);
+            Assert.Equal("{ message = Invalid patient data. }", badRequestResult.Value.ToString());
+        }
+
+        [Fact]
+        public async Task CreatePatient_ShouldReturnInternalServerError()
+        {
+            // Arrange
+            var patientDto = new PatientDto
+            {
+                MedicalRecordNumber = "P123",
+                FirstName = "John",
+                LastName = "Doe",
+                Email = "john.doe@example.com",
+                PhoneNumber = "+1234567890",
+                EmergencyContactName = "Jane Doe",
+                EmergencyContactPhoneNumber = "+351987654321",
+                DayOfBirth = "1",
+                MonthOfBirth = "1",
+                YearOfBirth = "1990",
+                Gender = "Male"
+            };
+
+            mockPatientService.Setup(service => service.CreatePatientAsync(It.IsAny<PatientDto>()))
+                              .ThrowsAsync(new Exception("An error occurred"));
+
+            // Act
+            var result = await controller.CreatePatient(patientDto);
+
+            // Assert
+            var internalServerErrorResult = Assert.IsType<ObjectResult>(result);
+            Assert.Equal(500, internalServerErrorResult.StatusCode);
+            Assert.Equal("{ message = An error occurred while processing your request., error = An error occurred }", internalServerErrorResult.Value.ToString());
+        }
+
         [Fact]
         public async Task DeletePatient_ShouldReturnOkStatus()
         {
             // Arrange
-            var mockPatientService = new Mock<IPatientService>();
-
             mockPatientService.Setup(service => service.DeletePatientAsync(It.IsAny<string>(), It.IsAny<string>()))
                               .ReturnsAsync(true);
 
-            var controller = new PatientController(mockPatientService.Object);
-
-            
             var user = new ClaimsPrincipal(new ClaimsIdentity(new Claim[]
             {
                 new Claim("custom:internalEmail", "admin@example.com")
@@ -47,14 +125,9 @@ namespace src.IntegrationTests
         public async Task DeletePatient_ShouldReturnNotFound()
         {
             // Arrange
-            var mockPatientService = new Mock<IPatientService>();
-
             mockPatientService.Setup(service => service.DeletePatientAsync(It.IsAny<string>(), It.IsAny<string>()))
                               .ReturnsAsync(false);
 
-            var controller = new PatientController(mockPatientService.Object);
-
-            
             var user = new ClaimsPrincipal(new ClaimsIdentity(new Claim[]
             {
                 new Claim("custom:internalEmail", "admin@example.com")
@@ -72,27 +145,25 @@ namespace src.IntegrationTests
             var notFoundResult = Assert.IsType<NotFoundObjectResult>(result);
             Assert.Equal("{ message = Patient not found. }", notFoundResult.Value.ToString());
         }
-       [Fact]
+        [Fact]
         public async Task UpdatePatient_ShouldReturnOkStatus()
         {
             // Arrange
-            var mockPatientService = new Mock<IPatientService>();
-
-            var Patient =    new Patient
-                {
-                    Id = new MedicalRecordNumber("2"),
-                    MedicalRecordNumber = new MedicalRecordNumber("2"),
-                    FirstName = new PatientFirstName("Jane"),
-                    LastName = new PatientLastName("Does"),
-                    FullName = new PatientFullName("Jane", "Does"),
-                    Email = new PatientEmail("Jane@email.com"),
-                    PhoneNumber = new PatientPhoneNumber("+351919991919"),
-                    EmergencyContact = new EmergencyContact("Jane", "+351919999119"),
-                    DateOfBirth = new DateOfBirth("01", "01", "1999"),
-                    Gender = Gender.Male,
-                    AllergiesAndConditions = new List<AllergiesAndConditions>(),
-                    AppointmentHistory = new AppointmentHistory()
-                };
+            var Patient = new Patient
+            {
+                Id = new MedicalRecordNumber("2"),
+                MedicalRecordNumber = new MedicalRecordNumber("2"),
+                FirstName = new PatientFirstName("Jane"),
+                LastName = new PatientLastName("Does"),
+                FullName = new PatientFullName("Jane", "Does"),
+                Email = new PatientEmail("Jane@email.com"),
+                PhoneNumber = new PatientPhoneNumber("+351919991919"),
+                EmergencyContact = new EmergencyContact("Jane", "+351919999119"),
+                DateOfBirth = new DateOfBirth("01", "01", "1999"),
+                Gender = Gender.Male,
+                AllergiesAndConditions = new List<AllergiesAndConditions>(),
+                AppointmentHistory = new AppointmentHistory()
+            };
             var patientDto = new PatientDto(Patient);
             mockPatientService.Setup(service => service.UpdatePatientAsync(It.IsAny<string>(), It.IsAny<PatientDto>()))
                               .ReturnsAsync(true);
@@ -100,11 +171,7 @@ namespace src.IntegrationTests
             mockPatientService.Setup(service => service.GetPatientByIdAsync(It.IsAny<string>()))
                               .Returns(Task.FromResult(patientDto));
 
-            var controller = new PatientController(mockPatientService.Object);
 
-
-
-            
             var user = new ClaimsPrincipal(new ClaimsIdentity(new Claim[]
             {
                 new Claim("custom:internalEmail", "admin@example.com")
@@ -115,7 +182,7 @@ namespace src.IntegrationTests
                 HttpContext = new DefaultHttpContext { User = user }
             };
 
-       
+
             // Act
             var result = await controller.UpdatePatient("P123456", patientDto);
 
@@ -123,27 +190,25 @@ namespace src.IntegrationTests
             var okResult = Assert.IsType<OkObjectResult>(result);
             Assert.Equal("{ message = Patient updated successfully. }", okResult.Value.ToString());
         }
-       [Fact]
+        [Fact]
         public async Task UpdatePatient_ShouldReturnNotFoundStatus()
         {
             // Arrange
-            var mockPatientService = new Mock<IPatientService>();
-
-            var Patient =    new Patient
-                {
-                    Id = new MedicalRecordNumber("2"),
-                    MedicalRecordNumber = new MedicalRecordNumber("2"),
-                    FirstName = new PatientFirstName("Jane"),
-                    LastName = new PatientLastName("Does"),
-                    FullName = new PatientFullName("Jane", "Does"),
-                    Email = new PatientEmail("Jane@email.com"),
-                    PhoneNumber = new PatientPhoneNumber("+351919991919"),
-                    EmergencyContact = new EmergencyContact("Jane", "+351919999119"),
-                    DateOfBirth = new DateOfBirth("01", "01", "1999"),
-                    Gender = Gender.Male,
-                    AllergiesAndConditions = new List<AllergiesAndConditions>(),
-                    AppointmentHistory = new AppointmentHistory()
-                };
+            var Patient = new Patient
+            {
+                Id = new MedicalRecordNumber("2"),
+                MedicalRecordNumber = new MedicalRecordNumber("2"),
+                FirstName = new PatientFirstName("Jane"),
+                LastName = new PatientLastName("Does"),
+                FullName = new PatientFullName("Jane", "Does"),
+                Email = new PatientEmail("Jane@email.com"),
+                PhoneNumber = new PatientPhoneNumber("+351919991919"),
+                EmergencyContact = new EmergencyContact("Jane", "+351919999119"),
+                DateOfBirth = new DateOfBirth("01", "01", "1999"),
+                Gender = Gender.Male,
+                AllergiesAndConditions = new List<AllergiesAndConditions>(),
+                AppointmentHistory = new AppointmentHistory()
+            };
             var patientDto = new PatientDto(Patient);
             mockPatientService.Setup(service => service.UpdatePatientAsync(It.IsAny<string>(), It.IsAny<PatientDto>()))
                               .ReturnsAsync(false);
@@ -151,11 +216,7 @@ namespace src.IntegrationTests
             mockPatientService.Setup(service => service.GetPatientByIdAsync(It.IsAny<string>()))
                               .Returns(Task.FromResult(patientDto));
 
-            var controller = new PatientController(mockPatientService.Object);
 
-
-
-            
             var user = new ClaimsPrincipal(new ClaimsIdentity(new Claim[]
             {
                 new Claim("custom:internalEmail", "admin@example.com")
@@ -166,7 +227,7 @@ namespace src.IntegrationTests
                 HttpContext = new DefaultHttpContext { User = user }
             };
 
-       
+
             // Act
             var result = await controller.UpdatePatient("P123456", patientDto);
 
@@ -174,24 +235,18 @@ namespace src.IntegrationTests
             var okResult = Assert.IsType<NotFoundObjectResult>(result);
             Assert.Equal("{ message = Patient not found. }", okResult.Value.ToString());
         }
-       [Fact]
+
+        [Fact]
         public async Task UpdatePatient_ShouldReturnBadRequestStatus()
         {
             // Arrange
-            var mockPatientService = new Mock<IPatientService>();
 
-            
             mockPatientService.Setup(service => service.UpdatePatientAsync(It.IsAny<string>(), It.IsAny<PatientDto>()))
                               .ReturnsAsync(false);
 
             mockPatientService.Setup(service => service.GetPatientByIdAsync(It.IsAny<string>()))
                               .Returns(Task.FromResult<PatientDto>(null));
 
-            var controller = new PatientController(mockPatientService.Object);
-
-
-
-            
             var user = new ClaimsPrincipal(new ClaimsIdentity(new Claim[]
             {
                 new Claim("custom:internalEmail", "admin@example.com")
@@ -202,7 +257,7 @@ namespace src.IntegrationTests
                 HttpContext = new DefaultHttpContext { User = user }
             };
 
-       
+
             // Act
             var result = await controller.UpdatePatient("P123456", null);
 
@@ -214,12 +269,9 @@ namespace src.IntegrationTests
         public async Task AcceptPatientPendingRequests_ShouldReturnOkStatus()
         {
             // Arrange
-            var mockPatientService = new Mock<IPatientService>();
 
             mockPatientService.Setup(service => service.AcceptRequests(It.IsAny<List<long>>()))
                               .ReturnsAsync(true);
-
-            var controller = new PatientController(mockPatientService.Object);
 
             // Act
             var result = await controller.AcceptPatientPendingRequests("1,2");
@@ -233,13 +285,9 @@ namespace src.IntegrationTests
         public async Task AcceptPatientPendingRequests_ShouldReturnNotFound()
         {
             // Arrange
-            var mockPatientService = new Mock<IPatientService>();
 
             mockPatientService.Setup(service => service.AcceptRequests(It.IsAny<List<long>>()))
                               .ReturnsAsync(false);
-
-            var controller = new PatientController(mockPatientService.Object);
-
 
             // Act
             var result = await controller.AcceptPatientPendingRequests("1,2");
@@ -252,11 +300,6 @@ namespace src.IntegrationTests
         [Fact]
         public async Task AcceptPatientPendingRequests_ShouldReturnBadRequest()
         {
-            // Arrange
-            var mockPatientService = new Mock<IPatientService>();
-
-            var controller = new PatientController(mockPatientService.Object);
-
             // Act
             var result = await controller.AcceptPatientPendingRequests(null);
 
@@ -264,6 +307,6 @@ namespace src.IntegrationTests
             var badRequestResult = Assert.IsType<BadRequestObjectResult>(result);
             Assert.Equal("{ message = Invalid pending requests data. }", badRequestResult.Value.ToString());
         }
-            
+
     }
 }
--- src/obj/Debug/net8.0/sem_5_24_25_043.AssemblyInfo.cs
+++ src/obj/Debug/net8.0/sem_5_24_25_043.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("sem_5_24_25_043")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03a3600d080dc2a1fa7a65b061d5b5a37865f326")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+006de540ce23f6aacd3c66aba60b7b9e3e07ab47")]
 [assembly: System.Reflection.AssemblyProductAttribute("sem_5_24_25_043")]
 [assembly: System.Reflection.AssemblyTitleAttribute("sem_5_24_25_043")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
