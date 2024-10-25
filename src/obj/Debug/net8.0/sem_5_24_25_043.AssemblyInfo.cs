763e6236be0aefd1308cbcae187d4aaefec87853
Ricardo <1220606@isep.ipp.pt>
2024-10-25 21:23:04 +0100

    Add End 2 End Tests for Staff, User and OperationType

--- src/obj/Debug/net8.0/sem_5_24_25_043.AssemblyInfo.cs
+++ src/obj/Debug/net8.0/sem_5_24_25_043.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("sem_5_24_25_043")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c8f1eae34d87f1a636f548da2418d59e74963cd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a28d409456fb9cadb259785f0c5a83a11f6be9f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("sem_5_24_25_043")]
 [assembly: System.Reflection.AssemblyTitleAttribute("sem_5_24_25_043")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
